7505445698fc9fd0197771c9302024757996c0ec
Luciabo <lucianoeduardo@live.com>
2018-06-09 11:55:19 -0300

    BinarySearchTree e seu Node implementados, BinarySearchTree insert e search implementados, falta remoção

--- src/application/BinarySearchTree.java
+++ src/application/BinarySearchTree.java
@@ -1,5 +1,48 @@
 package application;
 
 public class BinarySearchTree {
-
+	private BinarySearchTreeNode root;
+	
+	/**
+	 * Método construtor da classe
+	 */
+	public BinarySearchTree() {
+		root = null;	
+	}
+	
+	/**
+	 * Método para inserir na árvore
+	 * @param node: Nó da árvore onde deve-se começar o processo de inserção (por padrão deve ser a raiz).
+	 * @param word: Palavra a ser inserida na árvore.
+	 */
+	public void insert(BinarySearchTreeNode node, String word) {
+		if(node == null) {
+			node = new BinarySearchTreeNode(word);
+			return;
+		}
+		else if(node.getValue().compareTo(word)==0){
+			System.out.println("A palavra já está associada a um nó existente");
+			return;
+		}
+		else if(node.getValue().compareTo(word)>0){
+			insert(node.left, word);
+		}
+		else if(node.getValue().compareTo(word)<0) {
+			insert(node.right, word);
+		}
+	}
+	
+	public BinarySearchTreeNode search(BinarySearchTreeNode node, String word) {
+		if(node.getValue().compareTo(word)==0) {
+			return node;
+		}
+		else if(node.getValue().compareTo(word)>0){
+			search(node.left, word);
+		}
+		else if(node.getValue().compareTo(word)<0) {
+			search(node.right, word);
+		}
+		System.out.println("Palavra não encontrada");
+		return null;
+	}
 }
--- src/application/BinarySearchTreeNode.java
+++ src/application/BinarySearchTreeNode.java
@@ -1,5 +1,24 @@
 package application;
 
 public class BinarySearchTreeNode {
-
+	private String value; //nome do usuário
+	BinarySearchTreeNode left;
+	BinarySearchTreeNode right;
+	
+	/**
+	 * Método construtor do Nó da arvore
+	 */
+	public BinarySearchTreeNode(String nome) {
+		value = nome;
+		left = null;
+		right = null;
+	}
+	
+	/**
+	 * Método para retornar o valor de um nó
+	 */
+	public String getValue() {
+		return this.value;
+	}
+	
 }
